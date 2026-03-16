6598ea9873d8c86668d208acf4b6950d47cb05c0
murielgb <muriel.grosb@gmail.com>
2026-03-16 08:23:27 +0000

    Deploying to gh-pages from @ murielgb/murielgb.github.io@b8ab964d0a96aaae917a8e5e90d253ec1ad9e6ef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-teaching",
-          title: "Teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "nav-repositories",
           title: "Repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
